91a90db609d2eac7820f57be6ac8c0f9218f16d7
Yana Kozhokar <yanakozhokar1996@gmail.com>
2023-01-24 16:56:09 +0200

    updated

--- src/index.js
+++ src/index.js
@@ -1,92 +1,142 @@
-import Notiflix from 'notiflix';
 import axios from 'axios';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import './styles.css';
 
-const searchFormHandler = document.querySelector('#search-form');
-const inputRef = document.querySelector('.search-input');
-const galleryRef = document.querySelector('.gallery-list');
+let gallery = new SimpleLightbox('.gallery a');
+
+const URL_DEFAULT =
+  'https://pixabay.com/api/?key=32728160-634e7d154d1682a06810c8278';
+
+const formEl = document.querySelector('#search-form');
+const inputEl = document.querySelector('input[name="searchQuery"]');
+const divGalleryEl = document.querySelector('.gallery');
 const buttonMoreEl = document.querySelector('.load-more');
 const alertEl = document.querySelector('.text');
-
-searchFormHandler.addEventListener('submit', createGallery);
-buttonMoreEl.addEventListener('click', onButtonClick);
 
 let page = 1;
 let keyValue = '';
 let totalPages = 0;
 
-function createGallery(event) {
+formEl.addEventListener('submit', onFormSubmit);
+buttonMoreEl.addEventListener('click', onButtonClick);
+
+function onFormSubmit(event) {
   event.preventDefault();
-  keyValue = inputRef.value;
-  page = 1;
-  galleryRef.innerHTML = '';
+
+  keyValue = inputEl.value;
+
+  resetPage();
+  divGalleryEl.innerHTML = '';
   alertEl.classList.add('hidden');
+
   if (keyValue.trim() === '') {
-    Notiflix.Notify.info('Oops! Please, enter smth to search.');
+    Notify.info('Oops! Please, enter smth to search.');
     buttonMoreEl.classList.add('hidden');
     return;
   }
-  fetchImages(keyValue);
+
+  getImg(keyValue);
+
   event.currentTarget.reset();
 }
 
-async function fetchImages(input) {
+function onButtonClick() {
+  getImg(keyValue);
+}
+
+async function getImg(keyWord) {
   try {
     const response = await axios.get(
-      `https://pixabay.com/api/?key=32921127-0509bb2923ebc5e2476cd7059&q=$${input}&image_type=photo&orientation=horizontal&safesearch=true&fields=webformatURL,largeImageURL,tags,likes,views,comments,downloads&page=${page}&per_page=40`
+      `${URL_DEFAULT}&q=${keyWord}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`
     );
+
     if (response.data.hits.length === 0) {
-      Notiflix.Notify.failure(
+      Notify.failure(
         'Sorry, there are no images matching your search query. Please try again.'
       );
       buttonMoreEl.classList.add('hidden');
       alertEl.classList.add('hidden');
       return;
     }
+
     if (page === 1) {
-      Notiflix.Notify.success(
-        `Hooray! We found ${response.data.totalHits} images.`
-      );
+      Notify.success(`Hooray! We found ${response.data.totalHits} images.`);
     }
+
     buttonMoreEl.classList.remove('hidden');
     totalPages = Math.ceil(response.data.totalHits / 40);
-    createMarkup(response.data.hits);
+
+    renderGallery(response.data.hits);
     page += 1;
+
     if (page > totalPages) {
-      alertEl.classList.remove('hidden');
-      buttonMoreEl.classList.add('hidden');
+      return toogleAlertMarkup();
     }
   } catch (error) {
     console.error(error);
   }
 }
 
-function createMarkup(images) {
+function renderGallery(images) {
   const markup = images
     .map(image => {
-      return `<li class="gallery-item">
-      <div class="photo-card">
-        <img class="gallery-image" src="${image.webformatURL}" alt="${image.tags}" loading="lazy" />
-        <div class="info">
-          <p class="info-item">
-            <b>Likes</b> ${image.likes}
-          </p>
-          <p class="info-item">
-            <b>Views</b> ${image.views}
-          </p>
-          <p class="info-item">
-            <b>Comments</b> ${image.comments}
-          </p>
-          <p class="info-item">
-            <b>Downloads</b> ${image.downloads}
-          </p>
+      return `
+        <div class="photo-card">
+            <a href="${image.largeImageURL}">
+                <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" />
+            </a>
+            <div class="info">
+            <p class="info-item">
+            <b>Likes</b>
+            ${image.likes}
+            </p>
+            <p class="info-item">
+            <b>Views</b>
+            ${image.views}
+            </p>
+            <p class="info-item">
+            <b>Comments</b>
+            ${image.comments}
+            </p>
+            <p class="info-item">
+            <b>Downloads</b>
+            ${image.downloads}
+            </p>
+            </div>
         </div>
-      </div>
-    </li>`;
+        `;
     })
     .join('');
-  galleryRef.insertAdjacentHTML('beforeend', markup);
+  divGalleryEl.insertAdjacentHTML('beforeend', markup);
+
+  if (page > 1) {
+    const { height: cardHeight } = document
+      .querySelector('.gallery')
+      .firstElementChild.getBoundingClientRect();
+
+    window.scrollBy({
+      top: cardHeight * 2,
+      behavior: 'smooth',
+    });
+  }
+
+  gallery.refresh();
 }
 
-function onButtonClick() {
-  fetchImages(keyValue);
+gallery.on('show.simplelightbox', function () {
+  gallery.options.captionsData = 'alt';
+  gallery.options.captionDelay = 250;
+  gallery.options.scrollZoom = false;
+  gallery.options.scrollZoomFactor = 0;
+});
+
+function resetPage() {
+  page = 1;
+}
+
+function toogleAlertMarkup() {
+  alertEl.classList.remove('hidden');
+  buttonMoreEl.classList.add('hidden');
 }
